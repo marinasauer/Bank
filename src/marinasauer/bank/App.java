06b209acbd0c04516b52682034dd9b2f2e997ff5
marinasauer <marinarafaelasauer@gmail.com>
2023-01-31 14:31:59 -0300

    Cria entradas para o usuário inserir valores e opção de ações bancárias.

--- src/marinasauer/bank/App.java
+++ src/marinasauer/bank/App.java
@@ -1,16 +1,34 @@
 package marinasauer.bank;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-       Account account = new Account("Leoncio Martins","001","12345");
+        Scanner input = new Scanner(System.in);
+        Account account = new Account("Leoncio Martins","001","12345");
+
+        while (true){
+            System.out.println("Digite D para depósito, S para saque e E para sair");
+            String op = input.nextLine();
+            if (op.equalsIgnoreCase("d")){
+                System.out.println("Insira o valor que deseja depositar");
+                double value= input.nextDouble();
+                account.deposit(value);
+            }else if(op.equalsIgnoreCase("s")){
+                System.out.println("Insira o valor que deseja sacar");
+                double value = input.nextDouble();
+                account.withDraw(value);
+                }else if(op.equalsIgnoreCase("e")){
+                System.out.println(account.toString());
+                break;
+
+            }else{
+                System.out.println("Comando inválido");
+            }
+            input = new Scanner(System.in);
+            }
+        }
 
 
-       account.withDraw(200.00);
-       account.deposit(500.00);
-
-       account.deposit(1024.65);
-
-       System.out.println(account);
 
     }
-}
